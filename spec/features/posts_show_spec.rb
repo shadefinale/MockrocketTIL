9e3fefee408dd8184a76faa30afcf4a48ba29407
shadefinale <shadefinale@gmail.com>
2015-10-24 16:12:17 -0500

    Add posts show feature spec.

---
+++ spec/features/posts_show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+feature 'As a user, I want to be able to see a list of posts so that I can learn more things today' do
+  context 'user vists show path' do
+    before do
+      create(:post, title: "Single Post Test")
+      visit post_path(Post.last)
+    end
+
+    scenario 'user sees single post' do
+      expect(page).to have_content(Post.last.title)
+      expect(page).to_not have_content(build(:post).title)
+    end
+
+  end
+
+
+end
